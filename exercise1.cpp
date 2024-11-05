205409aab143b599b85033a6646f79c7d4d8743e
AININ SOFIYA BINTI ZUKHRI <ainin05@graduate.utm.my>
2024-11-06 01:22:32 +0800

    Add files via upload

---
+++ exercise1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //declaration of variables
+    int num1=50,num2=100;
+    int total;
+    //formula
+    total=num1+num2;
+    //display total
+    cout<<"total is "<<total;
+    
+   
+}
